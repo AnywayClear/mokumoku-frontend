bc804dcc882a2266acf5ac3a27bc1226c5ac5565
JeonJungyu <wjswnsrb93@naver.com>
2023-07-26 13:55:28 +0900

    feat: SpoqaHanSansNeo 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,50 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Dongle } from 'next/font/google';
+import localFont from 'next/font/local';
 
-const inter = Inter({ subsets: ['latin'] })
+const RobotoMono = Dongle({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
+
+const spokaFont = localFont({
+  src: [
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'MokuMoku',
   description: 'sell vegetable',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={spokaFont.className}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
-  )
+  );
 }
