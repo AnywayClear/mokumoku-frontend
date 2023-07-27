9df0b48317d7616a9c48460a3289bf2dd43cabbf
JeonJiHyeon <jihyean2004@naver.com>
2023-07-27 17:44:19 +0900

    feat. 소비자 등록 ㅠㅠ 너무 어렵네요..하..

--- src/app/consumer/register/page.tsx
+++ src/app/consumer/register/page.tsx
@@ -1,123 +1,113 @@
 'use client';
-import Button from '@/components/Button';
 import { useState, useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import DaumPostcode from 'react-daum-postcode';
+import Uploader from '@/components/Uploader';
+import { Button } from '@/components/Button';
 import axios from 'axios';
 
+const schema = yup
+  .object({
+    nickName: yup.string().max(10, '최대 10자까지 입력가능합니다.').required(),
+    image: yup.string().max(30, '최대 30자까지 입력 가능합니다.').required(),
+    address: yup.string().max(30, '최대 30자까지 입력 가능합니다').required(),
+  })
+  .required();
+
+type Inputs = {
+  nickName: string;
+  image: string;
+  address: string;
+};
+
+const LABEL_STYLE = 'leading-loose text-left block text-black text-sm w-3/12';
+const INPUT_STYLE =
+  'block box-border rounded-md w-full border-2 border-solid border-black py-2 px-3 text-sm text-black';
+
+const WRAPPER_INPUT_STYLE = 'w-9/12';
+
+const WRAPPER_STYLE = 'flex gap-4 my-4';
+
+const ERROR_STYLE = 'text-red-500 h-4 text-xs';
+const NICK_PCODE_STYLE =
+  'box-border h-4 w-40 p-4 border-2 align-middle mx-auto';
+const ADDRESS_STYLE = 'box-border h-4 w-80 p-4 border-2 align-middle mx-auto';
 export default function ConsumerRegister() {
   const [inputAddress, setInputAddressValue] = useState('');
   const [inputZipCodeValue, setInputZipCodeValue] = useState('');
   const [modalState, setModalState] = useState(false);
-  const [image, setImage] = useState({
-    image_file: '',
-    preview_URL: 'img/default_image.png',
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
   });
-
-  const Uploader = () => {
-    const [image, setImage] = useState({
-      image_file: '',
-      preview_URL: 'img/default_image.png',
-    });
-  };
-
-  const saveImage = (e: any) => {
-    e.preventDefault();
-    if (e.target.files[0]) {
-      // 새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
-      URL.revokeObjectURL(image.preview_URL);
-      const preview_URL = URL.createObjectURL(e.target.files[0]);
-      setImage(() => ({
-        image_file: e.target.files[0],
-        preview_URL: preview_URL,
-      }));
-    }
-  };
-
-  const deleteImage = () => {
-    // createObjectURL()을 통해 생성한 기존 URL을 폐기
-    URL.revokeObjectURL(image.preview_URL);
-    setImage({
-      image_file: '',
-      preview_URL: 'img/default_image.png',
-    });
-  };
-
-  useEffect(() => {
-    // 컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
-    return () => {
-      URL.revokeObjectURL(image.preview_URL);
-    };
-  }, []);
-
-  const sendImageToServer = async () => {
-    if (image.image_file) {
-      const formData = new FormData();
-      formData.append('file', image.image_file);
-      await axios.post('/api/image/upload', formData);
-      alert('서버에 등록이 완료되었습니다!');
-      setImage({
-        image_file: '',
-        preview_URL: 'img/default_image.png',
-      });
-    } else {
-      alert('사진을 등록하세요!');
-    }
-  };
+  console.log(errors);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   const modalToggle = () => {
     setModalState(!modalState);
   };
-  const postCodeStyle = {
+
+  useEffect(() => {
+    // modalState가 변경될 때마다 postCodeStyle을 업데이트
+    const postCodeStyle = {
+      width: '400px',
+      height: '400px',
+      display: modalState ? 'block' : 'none',
+    };
+    setPostCodeStyle(postCodeStyle);
+  }, [modalState]);
+
+  const [postCodeStyle, setPostCodeStyle] = useState({
     width: '400px',
     height: '400px',
-    display: modalState ? 'block' : 'none',
-  }; // 스타일 정의 code
+    display: 'none', // 초기 값은 모달이 닫혀있도록 설정
+  });
 
   const onCompletePost = (data: any) => {
     setInputAddressValue(data.address);
     setInputZipCodeValue(data.zonecode);
+    setModalState(false);
   }; // onCompletePost 함수
 
   return (
-    <div>
-      <h1>회원정보등록</h1>
-      <button
-        type="file"
-        accept="image/*"
-        onChange={saveImage}
-        // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
-        // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
-        onClick={(e) => (e.target.value = null)}
-        style={{ display: 'none' }}
-      ></button>
-      <div className="img-wrapper">
-        <img src={image.preview_URL} />
+    <form
+      className="flex flex-col w-9/12 align-middle mx-auto justify-center items-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="text-2xl font-extrabold text-center">회원정보등록</h2>
+      <hr />
+      <Uploader></Uploader>
+      <input className={NICK_PCODE_STYLE} placeholder="닉네임"></input>
+      <div className="flex gap-4 w-80">
+        <input
+          className={NICK_PCODE_STYLE}
+          value={inputZipCodeValue}
+          placeholder="우편번호"
+          type={'text'}
+        ></input>
+        <button onClick={modalToggle}>주소찾기</button>
       </div>
-
-      <div className="upload-button">
-        <button>Preview</button>
-        <button color="error" onClick={deleteImage}>
-          Delete
-        </button>
-        <button color="success" onClick={sendImageToServer}>
-          Upload
-        </button>
-      </div>
-
-      <input placeholder="닉네임"></input>
-      <input placeholder="배송지 검색"></input>
-      <Button onClick={modalToggle}>주소찾기</Button>
       <DaumPostcode
         style={postCodeStyle}
         onComplete={onCompletePost}
       ></DaumPostcode>
+
       <input
-        value={inputZipCodeValue}
-        placeholder="우편번호"
-        type={'text'}
+        className={ADDRESS_STYLE}
+        value={inputAddress}
+        placeholder="주소"
       ></input>
-      <input value={inputAddress} placeholder="주소"></input>
-      <input placeholder="상세주소를 입력해주세요"></input>
-    </div>
+      <input
+        className={ADDRESS_STYLE}
+        placeholder="상세주소를 입력해주세요"
+      ></input>
+      <button>등록하기</button>
+    </form>
   );
 }
---
+++ src/components/Uploader.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState, ChangeEvent } from 'react';
+import './uploader.css';
+import { Button } from '@mui/material';
+import axios from 'axios';
+import Image from 'next/image';
+
+const Uploader = () => {
+  const [image, setImage] = useState({
+    image_file: '' as File | string,
+    preview_URL: '/images/user.png',
+  });
+
+  let inputRef: HTMLInputElement | null;
+
+  const saveImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      URL.revokeObjectURL(image.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setImage({
+        image_file: e.target.files[0],
+        preview_URL: preview_URL,
+      });
+    }
+  };
+
+  useEffect(() => {
+    // 컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+    return () => {
+      URL.revokeObjectURL(image.preview_URL);
+    };
+  }, []);
+
+  const sendImageToServer = async () => {
+    if (image.image_file) {
+      const formData = new FormData();
+      formData.append('file', image.image_file);
+      await axios.post('/api/image/upload', formData);
+      alert('서버에 등록이 완료되었습니다!');
+      setImage({
+        image_file: '',
+        preview_URL: '/images/user.png',
+      });
+    } else {
+      alert('사진을 등록하세요!');
+    }
+  };
+
+  return (
+    <div className="uploader-wrapper">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={saveImage}
+        // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
+        // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
+        onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+          (e.currentTarget.value = '')
+        }
+        ref={(refParam) => (inputRef = refParam)}
+        style={{ display: 'none' }}
+      />
+      <div className="img-wrapper">
+        <Image
+          alt={image.preview_URL}
+          src={image.preview_URL}
+          width={120}
+          height={120}
+        />
+      </div>
+
+      <div className="upload-button">
+        <Button variant="contained" onClick={() => inputRef?.click()}>
+          이미지등록
+        </Button>
+        <Button color="success" variant="contained" onClick={sendImageToServer}>
+          Upload
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Uploader;
