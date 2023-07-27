d1fa521227b963a430d367ad25fb4f89b596420c
ssh102525 <ssh102525@naver.com>
2023-07-27 14:25:48 +0900

    feat: 마이페이지 page.tsx 추가

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,11 @@
+import BannerSelfConsumer from "@/components/MyPage/BannerSelfConsumer";
+import MyPageContent from "@/components/MyPage/MyPageContent";
+
+export default function MyPage(){
+    return(
+        <>
+            <BannerSelfConsumer></BannerSelfConsumer>
+            <MyPageContent></MyPageContent>
+        </>
+    );
+}
