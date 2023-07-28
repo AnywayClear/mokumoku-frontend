e82f6455954d3f8c3f0bc469021c1458c22423c4
JeonJungyu <wjswnsrb93@naver.com>
2023-07-28 17:48:58 +0900

    feat: 공통 api 메소드 추가

---
+++ src/service/api/http.ts
@@ -0,0 +1,19 @@
+import Axios from 'axios';
+const axios = Axios.create();
+
+const get = async (url: string) => {
+  const res = await axios.get<Response>(url);
+  return res.data;
+};
+
+const post = async (url: string, body?: Request) => {
+  const res = await axios.post<Response>(url, body);
+  return res.data;
+};
+
+const put = async (url: string, body?: Request) => {
+  const res = await axios.put<Response>(url, body);
+  return res.data;
+};
+
+const 
