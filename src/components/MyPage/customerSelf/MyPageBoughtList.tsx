3a95134758d94d38afacb48bd0bfb46ab2f6eb2d
ssh102525 <ssh102525@naver.com>
2023-07-31 09:58:58 +0900

    style: click event 요소 투명도 추가

--- src/components/MyPage/customerSelf/MyPageBoughtList.tsx
+++ src/components/MyPage/customerSelf/MyPageBoughtList.tsx
@@ -202,11 +202,11 @@ export default function BoughtList() {
                         {rows.map((row,index2)=>
                         (<tr key={index2} className="border-y text-lg">
                             <td>{row.img !== undefined ? <Image src={row.img} alt='상품이미지' width={300} height={300} className='h-32 w-80 object-cover' />:null}</td>
-                            <td><a href="#"><p className='px-6 underline truncate'>{row.title}</p></a></td>
+                            <td><a href="#"><p className='px-6 underline truncate hover:opacity-70'>{row.title}</p></a></td>
                             <td><p>{row.unit}</p></td>
                             <td><p>{row.price}</p></td>
                             <td><p>{row.date}</p></td>
-                            <td><a href=''><u>{row.deliv}</u></a></td>
+                            <td><a href=''><u className='hover:opacity-70'>{row.deliv}</u></a></td>
                             <td>{row.review?<button className='rounded-md bg-black text-white py-1 px-2' >후기작성</button>:<></>}</td>
                         </tr>))}
                     </tbody>
