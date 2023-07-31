c2b90f26d6447fd7ce68bae364dee70353bd03a4
ssh102525 <ssh102525@naver.com>
2023-07-31 15:35:07 +0900

    feat: Seller Content 레이아웃 추가

--- src/app/mypages/page.tsx
+++ src/app/mypages/page.tsx
@@ -1,9 +1,11 @@
+import SellerBannerSelf from "@/components/MyPage/sellerSelf/SellerBannerSelf";
+import SellerContent from "@/components/MyPage/sellerSelf/SellerContent";
 
 export default function MyPage(){
     return(
         <>
-            <BannerSelfSeller></BannerSelfSeller>
-            <MyPageContent></MyPageContent>
+            <SellerBannerSelf></SellerBannerSelf>
+            <SellerContent></SellerContent>
         </>
     );
 }
