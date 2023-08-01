ec54ba1e411dda9b03e04a8fa79c38cf7bb1ca73
ssh102525 <ssh102525@naver.com>
2023-08-01 17:19:57 +0900

    style: 판매자 배너, 판매자 설명 가로 길이 증가

--- src/components/MyPage/sellerSelf/SellerBannerSelf.tsx
+++ src/components/MyPage/sellerSelf/SellerBannerSelf.tsx
@@ -10,9 +10,9 @@ export default function SellerBannerSelf(){
                     <h1 className="text-4xl font-bold pb-4">마이페이지</h1>
                     <Image alt="MokuMoku" src={LogoImage} className="w-72 h-72 bg-white rounded-full"/>
                 </div>
-                <div className="text-left pt-16 w-96">
+                <div className="text-left pt-16 w-[27rem]">
                     <p className="text-5xl font-bold">하니네팜</p>
-                    <p className="text-2xl mt-4 overflow-hidden h-24 w-96">환영합니다! 저희 스토어는 신선하고 다양한 채소를 판매합니다. 건강한 매일 신선함을 전해드립니다</p>
+                    <p className="text-2xl mt-4 overflow-hidden h-24">환영합니다!<br/> 저희 스토어는 신선하고 다양한 채소를 판매합니다. 건강한 매일 신선함을 전해드립니다</p>
                     <p className="text-xl mt-4">사업자번호</p>
                     <p className="text-3xl font-bold">123-1234-1234</p>
                     <div className="flex pt-4">
