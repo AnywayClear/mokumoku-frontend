c28113a8e7d99efd47946e09606223fa0f44c97f
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-08-01 20:13:49 +0900

    fix : QueryProvider children 타입 추가

--- src/context/QueryProvider.tsx
+++ src/context/QueryProvider.tsx
@@ -4,7 +4,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-export default function QueryProvider({ children }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function QueryProvider({ children }: Props) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
