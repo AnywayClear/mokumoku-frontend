ef12b86a79ac24848668878117ec07ae1c51f09e
JeonJungyu <wjswnsrb93@naver.com>
2023-08-04 13:33:20 +0900

    feat : axios config 함수 추가

--- src/service/api/http.ts
+++ src/service/api/http.ts
@@ -10,22 +10,34 @@ const axios = Axios.create({
   // },
 });
 
+const getConfig = () => {
+  return {
+    headers: {
+      Authorization:
+        localStorage && localStorage.getItem('accessToken')
+          ? `Bearer ${localStorage.getItem('accessToken')}`
+          : null,
+      'Content-Type': 'application/json',
+    },
+  };
+};
+
 export const get = async (url: string) => {
-  const res = await axios.get<Response>(url);
+  const res = await axios.get<Response>(url, getConfig());
   return res.data;
 };
 
 export const post = async (url: string, body?: Request) => {
-  const res = await axios.post<Response>(url, body);
+  const res = await axios.post<Response>(url, body, getConfig());
   return res.data;
 };
 
 export const put = async (url: string, body?: Request) => {
-  const res = await axios.put<Response>(url, body);
+  const res = await axios.put<Response>(url, body, getConfig());
   return res.data;
 };
 
 export const del = async (url: string) => {
-  const res = await axios.delete<Response>(url);
+  const res = await axios.delete<Response>(url, getConfig());
   return res.data;
 };
