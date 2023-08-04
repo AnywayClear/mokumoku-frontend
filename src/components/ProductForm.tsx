de5527830826c865fc456c5c2f477a1707f70574
JeonJungyu <wjswnsrb93@naver.com>
2023-08-04 17:33:34 +0900

    feat : 이미지 s3에 업로드 추가

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -5,6 +5,7 @@ import * as yup from 'yup';
 import { FaPhotoVideo } from 'react-icons/fa';
 import { ChangeEvent, DragEvent, useState } from 'react';
 import Image from 'next/image';
+import AWS from 'aws-sdk';
 
 const schema = yup
   .object({
@@ -20,19 +21,28 @@ const schema = yup
     startPrice: yup
       .number()
       .min(1, '시작 가격은 1원 이상이어야 합니다.')
-      .required('필수 입력 항목입니다.'),
+      .required('필수 입력 항목입니다.')
+      .typeError('시작 가격을 입력해주세요.'),
     kg: yup
       .number()
       .min(9, '최소 9 글자를 입력해야 합니다.')
       .max(16, '최대 16자까지 입력 가능합니다.')
-      .required('무게는 1kg 이상이어야 합니다.'),
-    startDate: yup.date().required('필수 입력 항목입니다.'),
-    endDate: yup.date().required('필수 입력 항목입니다.'),
+      .required('무게는 1kg 이상이어야 합니다.')
+      .typeError('무게를 입력해주세요.'),
+    startDate: yup
+      .date()
+      .required('필수 입력 항목입니다.')
+      .typeError('시작시간을 입력해주세요.'),
+    endDate: yup
+      .date()
+      .required('필수 입력 항목입니다.')
+      .typeError('종료시간을 입력해주세요.'),
     ea: yup
       .number()
       .min(9, '최소 9 글자를 입력해야 합니다.')
       .max(16, '최대 16자까지 입력 가능합니다.')
-      .required('수량은 1개 이상이어야 합니다.'),
+      .required('수량은 1개 이상이어야 합니다.')
+      .typeError('수량을 입력해주세요.'),
   })
   .required();
 
@@ -59,7 +69,7 @@ const ERROR_STYLE = 'text-red-500 h-4 text-xs';
 
 export default function ProductForm() {
   const [dragging, setDragging] = useState(false);
-  const [file, setFile] = useState<File>();
+  const [file, setFile] = useState<File>(null);
 
   const {
     register,
@@ -109,130 +119,162 @@ export default function ProductForm() {
     }
   };
 
+  // s3 업로드 함수
+  const uploadS3 = (image: File) => {
+    AWS.config.update({
+      region: process.env.NEXT_PUBLIC_REGION,
+      accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY_ID,
+    });
+
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        ACL: 'public-read',
+        Bucket: 'mokumoku-image',
+        Key: `upload/${image.name}`,
+        Body: image,
+      },
+    });
+
+    upload.promise().then((data) => console.log(data));
+  };
+
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
-    <form className="flex flex-col w-9/12" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        className="hidden"
-        name="input"
-        id="input-upload"
-        type="file"
-        accept="image/*"
-        onChange={handleChange}
-      />
-      <label
-        className={`w-full h-60 flex flex-col items-center justify-center ${
-          !file && 'border-2 border-sky-500 border-dashed'
-        }`}
-        htmlFor="input-upload"
-        onDragEnter={handleDrag}
-        onDragLeave={handleDrag}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-      >
-        {dragging && (
-          <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none" />
-        )}
-        {!file && (
-          <div className="flex flex-col items-center pointer-events-none">
-            <FaPhotoVideo className="w-20 h-20 text-gray-300" />{' '}
-            <p>Drag and Drop your image here or click</p>
-          </div>
-        )}
-        {file && (
-          <div className="relative w-full aspect-square">
-            <Image
-              className="object-cover"
-              src={URL.createObjectURL(file)}
-              alt="local file"
-              fill
-              sizes="650px"
+    <>
+      <form className="flex flex-col w-9/12" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="hidden"
+          name="input"
+          id="input-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleChange}
+        />
+        <label
+          className={`w-full h-60 flex flex-col items-center justify-center ${
+            !file && 'border-2 border-sky-500 border-dashed'
+          }`}
+          htmlFor="input-upload"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          {dragging && (
+            <div className="absolute inset-0 z-10 bg-sky-500/20 pointer-events-none" />
+          )}
+          {!file && (
+            <div className="flex flex-col items-center pointer-events-none">
+              <FaPhotoVideo className="w-20 h-20 text-gray-300" />{' '}
+              <p>제품 사진을 등록해주세요.</p>
+            </div>
+          )}
+          {file && (
+            <div className="relative w-full aspect-square">
+              <Image
+                className="object-cover"
+                src={URL.createObjectURL(file)}
+                alt="local file"
+                fill
+                sizes="650px"
+              />
+            </div>
+          )}
+        </label>
+
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>제목</label>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              className={INPUT_STYLE}
+              placeholder="상품 제목"
+              {...register('name')}
             />
+            <p className={ERROR_STYLE}>{errors.name?.message}</p>
           </div>
-        )}
-      </label>
-
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>제목</label>
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input
-            className={INPUT_STYLE}
-            placeholder="상품 제목"
-            {...register('name')}
-          />
-          <p className={ERROR_STYLE}>{errors.name?.message}</p>
         </div>
-      </div>
 
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>내용</label>
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input
-            className={INPUT_STYLE}
-            placeholder="상품 설명"
-            {...register('desc')}
-          />
-          <p className={ERROR_STYLE}>{errors.desc?.message}</p>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>내용</label>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              className={INPUT_STYLE}
+              placeholder="상품 설명"
+              {...register('desc')}
+            />
+            <p className={ERROR_STYLE}>{errors.desc?.message}</p>
+          </div>
         </div>
-      </div>
 
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>시작가격</label>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>시작가격</label>
 
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input
-            type="number"
-            className={INPUT_STYLE}
-            placeholder="원"
-            {...register('startPrice')}
-          />
-          <p className={ERROR_STYLE}>{errors.startPrice?.message}</p>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              type="number"
+              className={INPUT_STYLE}
+              placeholder="원"
+              {...register('startPrice')}
+            />
+            <p className={ERROR_STYLE}>{errors.startPrice?.message}</p>
+          </div>
         </div>
-      </div>
 
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>무게</label>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>무게</label>
 
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input
-            type="number"
-            className={INPUT_STYLE}
-            placeholder="kg"
-            {...register('kg')}
-          />
-          <p className={ERROR_STYLE}>{errors.kg?.message}</p>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              type="number"
+              className={INPUT_STYLE}
+              placeholder="kg"
+              {...register('kg')}
+            />
+            <p className={ERROR_STYLE}>{errors.kg?.message}</p>
+          </div>
         </div>
-      </div>
 
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>수량</label>
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input className={INPUT_STYLE} placeholder="개" {...register('ea')} />
-          <p className={ERROR_STYLE}>{errors.ea?.message}</p>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>수량</label>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              className={INPUT_STYLE}
+              placeholder="개"
+              {...register('ea')}
+            />
+            <p className={ERROR_STYLE}>{errors.ea?.message}</p>
+          </div>
         </div>
-      </div>
 
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>경매 시작 시간</label>
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input
-            type="date"
-            className={INPUT_STYLE}
-            {...register('startDate')}
-          />
-          <p className={ERROR_STYLE}>{errors.startDate?.message}</p>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>경매 시작 시간</label>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              type="date"
+              className={INPUT_STYLE}
+              {...register('startDate')}
+            />
+            <p className={ERROR_STYLE}>{errors.startDate?.message}</p>
+          </div>
         </div>
-      </div>
-      <div className={WRAPPER_STYLE}>
-        <label className={LABEL_STYLE}>경매 종료 시간</label>
-        <div className={WRAPPER_INPUT_STYLE}>
-          <input type="date" className={INPUT_STYLE} {...register('endDate')} />
-          <p className={ERROR_STYLE}>{errors.endDate?.message}</p>
+        <div className={WRAPPER_STYLE}>
+          <label className={LABEL_STYLE}>경매 종료 시간</label>
+          <div className={WRAPPER_INPUT_STYLE}>
+            <input
+              type="date"
+              className={INPUT_STYLE}
+              {...register('endDate')}
+            />
+            <p className={ERROR_STYLE}>{errors.endDate?.message}</p>
+          </div>
         </div>
-      </div>
-      {/* {Object.keys(errors).length !== 0 && ( */}
-      <input className="cursor-pointer" type="submit" value="submit" />
-      {/* )} */}
-    </form>
+        {/* {Object.keys(errors).length !== 0 && ( */}
+        <input className="cursor-pointer" type="submit" value="submit" />
+        {/* )} */}
+      </form>
+
+      <div onClick={() => uploadS3(file)}>image upload button</div>
+    </>
   );
 }
