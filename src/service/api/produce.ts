60714e6b1f1f996e62f08ee3b5f2493dafe54f10
JeonJungyu <wjswnsrb93@naver.com>
2023-08-04 17:34:08 +0900

    feat : 농산물 호출 api 함수 추가

--- src/service/api/produce.ts
+++ src/service/api/produce.ts
@@ -1,11 +1,18 @@
-import { get, put } from './http';
+import { Produce } from '@/model/produce';
+import { get, getUrl, put } from './http';
 
 export const postProduces = async () => {
   await put('');
 };
 
 export const getProduceList = async (status: string) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/produces?statusNoList=${status}`;
+  const url = getUrl(`/api/produces?statusNoList=${status}`);
+  const data = await get(url);
+  return data;
+};
+
+export const getProduce = async (id: number): Promise<Produce> => {
+  const url = getUrl(`/api/produces/${id}`);
   const data = await get(url);
   return data;
 };
