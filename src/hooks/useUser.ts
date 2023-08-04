05d3d3ff4475b3b630727f28c0138bfd3d961c49
JeonJungyu <wjswnsrb93@naver.com>
2023-08-04 11:26:51 +0900

    feat : localstorage 저장 기능 제거

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,6 +1,5 @@
 import { AuthContext } from '@/context/AuthContext';
 import { useContext } from 'react';
-import { useLocalStorage } from './useLocalStorage';
 
 export interface User {
   id: string;
@@ -11,16 +10,13 @@ export interface User {
 
 export const useUser = () => {
   const { user, setUser } = useContext(AuthContext);
-  const { setItem } = useLocalStorage();
 
   const addUser = (user: User) => {
     setUser(user);
-    setItem('user', JSON.stringify(user));
   };
 
   const removeUser = () => {
     setUser(null);
-    setItem('user', '');
   };
 
   return { user, setUser, addUser, removeUser };
