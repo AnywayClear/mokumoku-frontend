fe54aee99b244429aebcd5075f6595b8a2c21f84
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-08-06 23:55:39 +0900

    style : 로딩 컴포넌트 스타일 변경

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,18 @@
+import { CSSProperties } from 'react';
+import CircleLoader from 'react-spinners/CircleLoader';
+
+const override: CSSProperties = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: 'red',
+};
+
 export default function Loading() {
   return (
-    <div>loading</div>
-  )
+    <div className="flex h-[60vh] justify-center items-center">
+      <div>
+        <CircleLoader size={100} color="#36d7b7" cssOverride={override} />
+      </div>
+    </div>
+  );
 }
