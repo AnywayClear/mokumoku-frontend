1a63107c084c49dc7be8d692777a6e216521f720
JeonJungyu <wjswnsrb93@naver.com>
2023-08-08 17:13:34 +0900

    feat : 인증 기능 업데이트

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,9 @@ import KaKaoLoginImage from '../../../public/images/kakao_login_medium_wide.png'
 import Image from 'next/image';
 
 export default function LoginPage() {
+  const handleClick = () =>
+    (location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth2/authorization/kakao`);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-3xl font-bold text-center border-b-4 border-b-black w-4/12 pb-4 mb-8">
@@ -13,13 +16,13 @@ export default function LoginPage() {
         alt="kakao"
         src={KaKaoLoginImage}
         className="cursor-pointer"
-        onClick={() =>
-          (location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth2/authorization/kakao`)
-        }
+        onClick={handleClick}
       />
       <div className="flex flex-col items-center mt-8">
         <p className="font-bold">아직 가입을 안하셨나요?</p>
-        <p className='text-sm underline'>카카오로 회원가입하기</p>
+        <p className="text-sm underline cursor-pointer" onClick={handleClick}>
+          카카오로 회원가입하기
+        </p>
       </div>
     </div>
   );
