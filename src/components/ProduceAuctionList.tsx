6ba244c9a37e738b547eeafaee654815d00a61e0
JeonJungyu <wjswnsrb93@naver.com>
2023-08-08 17:17:13 +0900

    feat : 경매리스트 업데이트

--- src/components/ProduceAuctionList.tsx
+++ src/components/ProduceAuctionList.tsx
@@ -55,40 +55,35 @@ const people = [
   },
 ];
 
-
-
 type Props = {
-  auctionList: AuctionList | undefined;
+  auctionList: AuctionList[] | undefined;
 };
 
 export default function ProduceAuctionList({ auctionList }: Props) {
   return (
     <ul role="list" className="divide-y divide-gray-100">
-      {people.map((person) => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
+      {auctionList?.map((auction) => (
+        <li key={auction.id} className="flex justify-between gap-x-6 py-5">
           <div className="flex min-w-0 gap-x-4">
             {/* <img
               className="h-12 w-12 flex-none rounded-full bg-gray-50"
-              src={person.imageUrl}
+              src={auction.imageUrl}
               alt=""
             /> */}
             <div className="min-w-0 flex-auto">
               <p className="text-sm font-semibold leading-6 text-gray-900">
-                {person.name}
+                {auction.id}
               </p>
               <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                {person.email}
+                {auction.id}
               </p>
             </div>
           </div>
           <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-            {person.lastSeen ? (
+            <p className="text-sm leading-6 text-gray-900">{auction.id}</p>
+            {auction.id ? (
               <p className="mt-1 text-xs leading-5 text-gray-500">
-                Last seen{' '}
-                <time dateTime={person.lastSeenDateTime}>
-                  {person.lastSeen}
-                </time>
+                Last seen <time>{auction.price}</time>
               </p>
             ) : (
               <div className="mt-1 flex items-center gap-x-1.5">
