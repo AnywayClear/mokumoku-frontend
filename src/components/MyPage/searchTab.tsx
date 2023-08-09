857a37749761f76d1d4ff12000aac67f8c65d4c2
ssh102525 <ssh102525@naver.com>
2023-08-10 00:37:53 +0900

    feat: search tab 컴포넌트화

--- src/components/MyPage/searchTab.tsx
+++ src/components/MyPage/searchTab.tsx
@@ -1,11 +1,11 @@
 import { searchType } from "@/model/mypage";
-import { dateSelector, searchState } from "@/store/mypage";
+import { searchState } from "@/store/mypage";
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 import { useState } from "react";
-import { useRecoilState, useResetRecoilState } from "recoil";
+import { useRecoilState} from "recoil";
 
 type Props={
     tabType:number;
@@ -29,17 +29,19 @@ const chipStyle:chipStyleType = {
     hover:"hover:text-green-500 hover:border-green-500 hover:border-[3px] hover:px-[9px] hover:py-[2px] "
 };
 
+function processDate(dateStr: string | null) { 
+    if (!dateStr) {
+        return null;
+    } else { 
+        return dayjs(dateStr);
+    }
+}
+
 export default function SearchTab({tabType}:Props){
 
-    type test={
-        formattedStartDate:Dayjs|null,
-        formattedEndDate:Dayjs|null
-    }
-    
-    const [{title, auctionState,dateState, startDate, endDate, orderBy}, setStatus] = useRecoilState<searchType>(searchState);
-    const [{formattedStartDate, formattedEndDate}, setFormattedData] = useRecoilState<test>(dateSelector);
+    const [{title, auctionState,dateState, startDateStr, endDateStr, orderBy}, setStatus] = useRecoilState<searchType>(searchState);
     const [titleText, setTitleText] = useState<string>('');
-    
+
     let auctionStateArr:string[] = [];
     
     if(tabType==0){
@@ -47,7 +49,6 @@ export default function SearchTab({tabType}:Props){
     }else if(tabType==1){
         auctionStateArr  = ["경매전","경매중","경매후"];
     }
-
 
     function changeAutionState(num:number){
         if(num===auctionState){
@@ -61,46 +62,47 @@ export default function SearchTab({tabType}:Props){
 
         if(num==dateState){
             setStatus((current)=>({...current, dateState:null}));
-            setStatus((current)=>({...current, startDate:null}));
-            setStatus((current)=>({...current, endDate:null}));
+            setStatus((current)=>({...current, startDateStr:null}));
+            setStatus((current)=>({...current, endDateStr:null}));
 
         }else{
             setStatus((current)=>({...current, dateState:num}));
-            setStatus((current)=>({...current, endDate:dayjs().toString()}));
+            setStatus((current)=>({...current, endDateStr:dayjs().toString()}));
 
             if(num===0){
-                setStatus((current)=>({...current, startDate:(dayjs().subtract(1,"day").toString())}));
+                setStatus((current)=>({...current, startDateStr:(dayjs().subtract(1,"day").toString())}));
             }else if(num===1){
-                setStatus((current)=>({...current, startDate:(dayjs().subtract(30,"day").toString())}));
+                setStatus((current)=>({...current, startDateStr:(dayjs().subtract(30,"day").toString())}));
             }else if(num===2){
-                setStatus((current)=>({...current, startDate:(dayjs().subtract(365,"day").toString())}));
+                setStatus((current)=>({...current, startDateStr:(dayjs().subtract(365,"day").toString())}));
             }
         }
     }
 
-    //새로운 시작일이 종료일보다 빠르면 종료일을 시작일로 설정
-    //종료일이 현재일이고 1일, 30일, 1년 차이면 setDateState
-    //그외엔 setDateState(-1)
     function changeStartDate(newDate:Dayjs|null){
-
-        if(newDate!==null){
-            if(newDate.isAfter(dayjs())) newDate = dayjs();
-            setStatus((current)=>({...current, startDate:(newDate==null?null:newDate.toString())}));
+        
+        let endDate = processDate(endDateStr);
+        let chkNewDate: string | null = null;
+        
+        if (newDate!==null) { 
+            chkNewDate = newDate.isAfter(dayjs()) ? dayjs().toString() : newDate.toString();
         }
+        
+        setStatus((current)=>({...current, startDateStr:chkNewDate}));
 
-        if(formattedEndDate !== null){
-            if(formattedEndDate.isBefore(newDate)){
-                setStatus((current)=>({...current, endDate:(newDate==null?null:newDate.toString())}));
+        if(endDate !== null){
+            if(endDate.isBefore(newDate)){
+                setStatus((current)=>({...current, endDateStr:chkNewDate}));
             }
 
             setStatus((current)=>({...current, dateState:-1}));
 
-            if(formattedEndDate.diff(dayjs(),'day')==0){
-                if(formattedEndDate.diff(newDate,'day')===1){
+            if(endDate.diff(dayjs(),'day')==0){
+                if(endDate.diff(newDate,'day')===1){
                     setStatus((current)=>({...current, dateState:0}));
-                }else if(formattedEndDate.diff(newDate,'day')===30){
+                }else if(endDate.diff(newDate,'day')===30){
                     setStatus((current)=>({...current, dateState:1}));
-                }else if(formattedEndDate.diff(newDate,'day')===365){
+                }else if(endDate.diff(newDate,'day')===365){
                     setStatus((current)=>({...current, dateState:2}));
                 }
             }
@@ -108,30 +110,32 @@ export default function SearchTab({tabType}:Props){
         
     }
 
-    //새로운 종료일이 오늘보다 늦으면 그냥 오늘로 설정
-    //새로운 종료일이 시작일보다 빠르면 시작일을 종료일로 설정
-    //종료일이 현재일이고 1일, 30일, 1년 차이면 setDateState
-    //그외엔 setDateState(-1)
-    function changeEndDate(newDate:Dayjs|null){
+    function changeEndDate(newDate: Dayjs | null) {
+        
+        let endDate = processDate(endDateStr);
+        let startDate = processDate(startDateStr);
+        let chkNewDate: string | null = null;
 
-        if(newDate!==null){
-            if(newDate.isAfter(dayjs())) newDate = dayjs();
-            setStatus((current)=>({...current, endDate:(newDate==null?null:newDate.toString())}));
+        if (newDate!==null) { 
+            chkNewDate = newDate.isAfter(dayjs()) ? dayjs().toString() : newDate.toString();
         }
+        
+        setStatus((current)=>({...current, endDateStr:chkNewDate}));
+        
 
         if(startDate !== null){
-            if(formattedStartDate?.isAfter(newDate)){
-                setStatus((current)=>({...current, startDate:(newDate==null?null:newDate.toString())}));
+            if(startDate?.isAfter(newDate)){
+                setStatus((current)=>({...current, startDateStr:chkNewDate}));
             }
 
             setStatus((current)=>({...current, dateState:-1}));
 
             if(newDate !== null && newDate.diff(dayjs(),"day")==0){
-                if(newDate.diff(formattedStartDate,'day')===1){
+                if(newDate.diff(startDate,'day')===1){
                     setStatus((current)=>({...current, dateState:0}));
-                }else if(newDate.diff(formattedStartDate,'day')===30){
+                }else if(newDate.diff(startDate,'day')===30){
                     setStatus((current)=>({...current, dateState:1}));
-                }else if(newDate.diff(formattedStartDate,'day')===365){
+                }else if(newDate.diff(startDate,'day')===365){
                     setStatus((current)=>({...current, dateState:2}));
                 }
             }
@@ -142,56 +146,57 @@ export default function SearchTab({tabType}:Props){
         setStatus((current)=>({...current, orderBy:event.target.value}));
     };
 
-    return(
-        <>
-            {   
-                tabType===0 ||tabType===1?
-                <> 
-                    <div className="flex space-x-2 mt-8">
-                        {auctionStateArr.map((autcionStateArrItem, index)=>
-                        <button key={index} className={(index===auctionState?chipStyle.on:chipStyle.off)+chipStyle.common +chipStyle.hover } onClick={()=>changeAutionState(index)}>{autcionStateArrItem}</button>
-                        )}
-                    </div>
-                    <div className="flex justify-between">
-                        <div className="flex space-x-2 items-center">
-                          <div className="flex space-x-2 mr-4">
-                              {dateStateArr.map((dateStateArrItem, index)=>
-                               <button key={index} className={`${(index===dateState?chipStyle.on:chipStyle.off)+chipStyle.common + chipStyle.hover} `} onClick={()=>changeDateState(index)}>{dateStateArrItem}</button>
-                                 )}
-                         </div>
+    
+    if (tabType <= 2) {
+        return (
+            <div className="mt-12">{tabType<=1?
+                <div className="flex space-x-2">
+                    {auctionStateArr.map((autcionStateArrItem, index) =>
+                        <button key={index} className={(index === auctionState ? chipStyle.on : chipStyle.off) + chipStyle.common + chipStyle.hover} onClick={() => changeAutionState(index)}>{autcionStateArrItem}</button>
+                    )}
+                </div>: null
+                }
+                <div className="flex justify-between">
+                    <div className="flex space-x-2 items-center">
+                        <div className="flex space-x-2 mr-4">
+                            {dateStateArr.map((dateStateArrItem, index) =>
+                                <button key={index} className={`${(index === dateState ? chipStyle.on : chipStyle.off) + chipStyle.common + chipStyle.hover} `} onClick={() => changeDateState(index)}>{dateStateArrItem}</button>
+                            )}
+                        </div>
                         <div className="flex items-center">
                             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker  
-                                    label="시작일" 
-                                    slotProps={{ textField: { size: 'small' } }} 
-                                    value={startDate}
-                                    onChange={(newValue) => changeStartDate(dayjs(newValue))} 
+                                <DatePicker
+                                    label="시작일"
+                                    slotProps={{ textField: { size: 'small' } }}
+                                    // value={processDate(formattedStartDate)}
+                                    value={processDate(startDateStr)}
+                                    onChange={(newValue) => changeStartDate(newValue)}
                                     className="w-44"
                                 />
                                 <p className="text-bold text-xl mx-1">~</p>
                                 <DatePicker
-                                    label="종료일" 
+                                    label="종료일"
                                     slotProps={{ textField: { size: 'small' } }}
-                                    value={endDate}
-                                    onChange={(newValue) => changeEndDate(dayjs(newValue))} 
+                                    // value={processDate(formattedEndDate)}
+                                    value={processDate(endDateStr)}
+                                    onChange={(newValue) => changeEndDate(newValue)}
                                     className="w-44"
                                 />
-                            </LocalizationProvider>  
+                            </LocalizationProvider>
                         </div>
                         <TextField
-                               label="물품이름 검색"
-                               size="small"
-                               className="w-52 ml-2 mr-1"
-                               value={titleText}
-                               onChange={(event)=>setTitleText(event.target.value)}
-                            />
-                        <button 
-                                className="hover:opacity-70 bg-white text-neutral-800 text-lg font-semibold rounded-md px-4 p-[0.28rem] border-2 border-neutral-300"
-                                onClick={()=>{
-                                    setStatus((current)=>({...current, title:titleText}));
-                                    console.log(title);
-                                }}
-                                >검색
+                            label="물품이름 검색"
+                            size="small"
+                            className="w-52 ml-2 mr-1"
+                            value={titleText}
+                            onChange={(event) => setTitleText(event.target.value)}
+                        />
+                        <button
+                            className="hover:opacity-70 bg-white text-neutral-800 text-lg font-semibold rounded-md px-4 p-[0.28rem] border-2 border-neutral-300"
+                            onClick={() => {
+                                setStatus((current) => ({ ...current, title: titleText }));
+                            }}
+                        >검색
                         </button>
                     </div>
                     <div className='font-bold'>
@@ -203,49 +208,46 @@ export default function SearchTab({tabType}:Props){
                                 value={orderBy}
                                 label="정렬기준"
                                 onChange={handleChange}
-                                inputProps={{MenuProps: {disableScrollLock: true}}}
+                                inputProps={{ MenuProps: { disableScrollLock: true } }}
                             >
-                                {orderStateArr.map((orderStateArrItem,index)=><MenuItem value={index} key={index}>{orderStateArrItem}</MenuItem>)}
+                                {orderStateArr.map((orderStateArrItem, index) => <MenuItem value={index} key={index}>{orderStateArrItem}</MenuItem>)}
                             </Select>
                         </FormControl>
                     </div>
                 </div>
-            </>
-            :
-            
-                <div className="flex items-center justify-end">
-                    <TextField
-                            label="물품이름 검색"
-                            size="small"
-                            className="w-52 ml-2 mr-1"
-                            value={titleText}
-                            onChange={(event)=>setTitleText(event.target.value)}
-                        />
-                    <button 
-                            className="hover:opacity-70 bg-white text-neutral-800 text-lg font-semibold rounded-md px-4 p-[0.28rem] border-2 border-neutral-300"
-                            onClick={()=>{
-                                setStatus((current)=>({...current, title:titleText}));
-                            }}
-                            >검색
-                        </button>
-                <div className='font-bold'>
-                        <FormControl size="small" sx={{ m: 1, minWidth: 120 }}>
-                            <InputLabel id="demo-select-small-label">정렬기준</InputLabel>
-                            <Select
-                                labelId="demo-select-small-label"
-                                id="demo-select-small"
-                                value={orderBy}
-                                label="정렬기준"
-                                onChange={handleChange}
-                                inputProps={{MenuProps: {disableScrollLock: true}}}
-                            >
-                                {orderStateArr.map((orderStateArrItem,index)=><MenuItem value={index} key={index}>{orderStateArrItem}</MenuItem>)}
-                            </Select>
-                        </FormControl>
-                </div>
             </div>
-            
-            }
-        </>
-    );
+        );
+    } else {
+        return (<div className="flex items-center justify-end">
+            <TextField
+                label="물품이름 검색"
+                size="small"
+                className="w-52 ml-2 mr-1"
+                value={titleText}
+                onChange={(event) => setTitleText(event.target.value)}
+            />
+            <button
+                className="hover:opacity-70 bg-white text-neutral-800 text-lg font-semibold rounded-md px-4 p-[0.28rem] border-2 border-neutral-300"
+                onClick={() => {
+                    setStatus((current) => ({ ...current, title: titleText }));
+                }}
+            >검색
+            </button>
+            <div className='font-bold'>
+                <FormControl size="small" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-select-small-label">정렬기준</InputLabel>
+                    <Select
+                        labelId="demo-select-small-label"
+                        id="demo-select-small"
+                        value={orderBy}
+                        label="정렬기준"
+                        onChange={handleChange}
+                        inputProps={{ MenuProps: { disableScrollLock: true } }}
+                    >
+                        {orderStateArr.map((orderStateArrItem, index) => <MenuItem value={index} key={index}>{orderStateArrItem}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            </div>
+        </div>);
+    }
 }
