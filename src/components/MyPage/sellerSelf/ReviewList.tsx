9fcc5b44aec2376c0e2432bf16f3245fb6e32fd6
ssh102525 <ssh102525@naver.com>
2023-08-10 17:55:03 +0900

    style: 리뷰 목록 스타일 추가

--- src/components/MyPage/sellerSelf/ReviewList.tsx
+++ src/components/MyPage/sellerSelf/ReviewList.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import Image from 'next/image';
 import { BsStar, BsStarFill } from 'react-icons/bs';
 import SearchTab from '../searchTab';
-import { jsx } from '@emotion/react';
 
 type reviewType={
     img: string,
@@ -76,6 +75,42 @@ export default function ReviewList(){
                                 <p className='text-2xl font-bold mb-2'>{review.nickname}</p>
                                 <p className='text-lg'>{review.content}</p>
                             </div>
+                        </tr>
+                    </>
+                )
+                }
+            </table>
+            <table className=''>
+                {
+                    reviews.map((review, index)=>
+                    <>
+                        <tr className='border-y-2 border-neutral-300 border-collapse' key={index}>
+                            <div className='flex justify-between h-fit items-top my-6' >
+                                <Image className='h-40 w-60 object-cover' src={review.img} alt="이미지입니다." width={250} height={120}/>
+                                <div className='ml-7'>
+                                    <div className='flex justify-between'>
+                                        <div className='w-full'>
+                                            <div className='flex items-end'>
+                                                <a href="#"><p className='font-bold text-2xl mr-2 underline underline-offset-4'>{review.title}</p></a>
+                                                <p className='text-lg text-gray-600 mr-2'>{review.unit}</p>
+                                            </div>
+                                        </div>
+                                        <div className='w-1/5 text-right text-lg text-gray-600'>
+                                                <p className='my-auto'>{review.date}</p>
+                                            </div>
+                                    </div>
+                                    <div className="mt-4">
+                                        <div className="flex items-center mb-2">
+                                            <p className='text-xl font-bold mr-2'>{review.nickname}</p>
+                                            <div className='flex items-start text-xl text-yellow-300'>
+                                                    { getStar(review.rate) }
+                                            </div>
+                                        </div>
+                                        <p className='text-lg text-justify'>{review.content}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
                         </tr>
                     </>
                 )
