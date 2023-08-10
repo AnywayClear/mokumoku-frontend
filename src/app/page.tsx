73c6095a471f06cb60b85c706edbc7bb4819a540
JeonJungyu <wjswnsrb93@naver.com>
2023-08-10 17:33:04 +0900

    feat : 메인 페이지 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,12 @@ import Image from 'next/image';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
+import MainBoxImage from '../../public/images/main/main_box.svg';
+import MainDownImage from '../../public/images/main/main_down.svg';
+import MainLeafImage from '../../public/images/main/main_leaf.svg';
+
 export default function Home() {
   const { user } = useContext(AuthContext);
-
 
   // const { data: produce }: UseQueryResult<Produce> = useQuery({
   //   queryKey: ['produce', slug],
@@ -56,6 +59,49 @@ export default function Home() {
         </Carousel>
       </div>
       <h1>main</h1>
+      <div className="flex gap-4 justify-center">
+        <div className="flex flex-col justify-center items-center">
+          <div className="relative w-[68px] h-[68px] border-2 border-black rounded-full">
+            <Image
+              alt="logo"
+              className="object-cover rounded-full w-16 h-16 p-[0.2rem]"
+              fill
+              src={MainBoxImage}
+            />
+          </div>
+          <p className="text-lg font-bold leading-6 text-gray-900">
+            가장 소량씩, 가장 신성하게
+          </p>
+          <p className="text-sm font-base leading-6 text-gray-400">
+            적은 양으로 부담없이 즐겨요.</p>
+        </div>
+
+        <div>
+          <div className="relative w-[68px] h-[68px] border-2 border-black rounded-full">
+            <Image
+              alt="logo"
+              className="object-cover rounded-full w-16 h-16 p-[0.2rem]"
+              fill
+              src={MainDownImage}
+            />
+          </div>
+          <p>친환경 채소를 가장 저렴하게!</p>
+          <p>30% OFF, 합리적인 가격으로 만나요!</p>
+        </div>
+
+        <div>
+          <div className="relative w-[68px] h-[68px] border-2 border-black rounded-full">
+            <Image
+              alt="logo"
+              className="object-cover rounded-full w-16 h-16 p-[0.2rem]"
+              fill
+              src={MainLeafImage}
+            />
+          </div>
+          <p>환경에 한 발 나은 선택</p>
+          <p>환경에 더 가까운 생산과 소비를 지지해요.</p>
+        </div>
+      </div>
     </>
   );
 }
