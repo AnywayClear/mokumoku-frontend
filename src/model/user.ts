d91e0101e4add30b436d14d3fa5e8ae30e031e82
ssh102525 <ssh102525@naver.com>
2023-08-11 14:48:07 +0900

    feat: userData type 값 추가

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,4 +1,9 @@
 export type userData={
     nickname: string|null,
-    image: string|null
+    image: string|null,
+    description:string|null,
+    emailAddress:string|null,
+    companyAddress:string|null,
+    companyRegistrationNumber:string|null,
+    phoneNumber:string|null,
 }
