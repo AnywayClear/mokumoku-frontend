6d0c6f546c724bc7a2c31ffc60178fc123c6c9ab
ssh102525 <ssh102525@naver.com>
2023-08-11 17:36:04 +0900

    style: tsx파일 스타일에 맞게 수정 및 폴더 위치 변경

--- src/components/MyPage/customerSelf/CustomerBox.tsx
+++ src/components/MyPage/customerSelf/CustomerBox.tsx
@@ -1,37 +1,36 @@
-import { useRecoilState } from "recoil";
-import BoughtList from "./BoughtList";
-import SubscribeList from "./SubscribeList";
-import WishList from "./WishList";
-import { tapState } from "@/store/mypage";
+import { useRecoilState } from 'recoil';
+import BoughtList from './BoughtList/BoughtList';
+import SubscribeList from './SubscribeList/SubscribeList';
+import WishList from './WishList/WishList';
+import { tapState } from '@/store/mypage';
 
 const boxItems = [
-    {
-        tag : <BoughtList/>,
-        number : 0
-    },
-    {
-        tag : <WishList/>,
-        number : 1
-    },
-    {
-        tag : <SubscribeList/>,
-        number : 2
-    },
+  {
+    tag: <BoughtList />,
+    number: 0,
+  },
+  {
+    tag: <WishList />,
+    number: 1,
+  },
+  {
+    tag: <SubscribeList />,
+    number: 2,
+  },
 ];
 
 type Props = {
-    boxItemNum: number;
-}
+  boxItemNum: number;
+};
 
-export default function CustomerBox(){
+export default function CustomerBox() {
+  const [status, setStatus] = useRecoilState<number>(tapState);
 
-    const [status, setStatus] = useRecoilState<number>(tapState);
-
-    return (
-        <div className="border-neutral-500 mb-32">
-            {boxItems.map((boxItem,index)=>(
-                status===boxItem.number?boxItem.tag:null
-            ))}
-        </div>
-    );
+  return (
+    <div className="border-neutral-500 mb-32">
+      {boxItems.map((boxItem, index) =>
+        status === boxItem.number ? boxItem.tag : null,
+      )}
+    </div>
+  );
 }
