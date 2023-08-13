dff7c6ceed5a118d2bc8b36979596cfda8a37c8d
ssh102525 <ssh102525@naver.com>
2023-08-14 05:16:59 +0900

    feat: 구매자 마이페이지 찜목록 api 호출

--- src/components/MyPage/customerSelf/WishList/WishRow.tsx
+++ src/components/MyPage/customerSelf/WishList/WishRow.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import Image from 'next/image';
 import { ImHeartBroken } from 'react-icons/im';
-
-type rowType = {
-    id:number,
-    img?:string,
-    title:string,
-    seller:string,
-    unit:string,
-    price:number,
-    date:string
-};
+import { Produce } from '@/model/produce';
+import { getProduce } from '@/service/api/produce';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { dateToString } from '@/myFunc';
 
 type Props = {
-    row:rowType;
+    id: number;
 }
 
-export default function WishRow({row}:Props) {
+export default function WishRow({ id }: Props) {
+    
+    const { data: produce }: UseQueryResult<Produce> = useQuery({
+        queryKey: ['produce'],
+        queryFn: () => getProduce(id),
+      });
+
   return (
     <>
         <tr className="border-y text-lg">
-            <td>{row.img !== undefined ? <Image src={row.img} alt='상품이미지' width={300} height={300} className='h-32 w-80 object-cover' />:null}</td>
+            <td>{produce?.image !== undefined ? <Image src={produce?.image} alt='상품이미지' width={300} height={300} className='h-32 w-80 object-cover' />:null}</td>
             <td>
                 <a href="#">
-                    <p className='px-6 underline truncate hover:opacity-70'>{row.title}</p>
+                    <p className='px-6 underline truncate hover:opacity-70'>{produce?.name}</p>
                 </a>
             </td>
             <td>
                 <a href="#" className='w-full h-full'>
-                    <u className="hover:opacity-70">{row.seller}</u>
+                    <u className="hover:opacity-70">{produce?.seller}</u>
                 </a>
             </td>
-            <td><p>{row.unit}</p></td>
-            <td><p>{row.date}</p></td>
-            <td><p>{row.price}</p></td>
+            <td><p>{produce?.ea}</p></td>
+            <td><p>{produce?dateToString(produce.startDate):null}</p></td>
+            <td><p>{produce?.startPrice}</p></td>
             <td>
                 <a className="hover:opacity-30 p-6" href="#">
                     <ImHeartBroken className="mx-auto"/>
