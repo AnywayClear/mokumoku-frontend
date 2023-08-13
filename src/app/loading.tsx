9b2cbac8684ff00f885030854c9df3c25b2aa95a
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-08-13 18:02:49 +0900

    fix : 로딩 페이지 버그 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CSSProperties } from 'react';
 import GridLoader from 'react-spinners/GridLoader';
 
@@ -11,7 +13,7 @@ export default function Loading() {
   return (
     <div className="flex h-[60vh] justify-center items-center">
       <div>
-        <GridLoader size={130} color="#4ade80" cssOverride={override} />
+        <GridLoader size={100} color="#4ade80" cssOverride={override} />
       </div>
     </div>
   );
