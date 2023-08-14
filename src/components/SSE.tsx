0ba832980485eb442befe5ba98fb49288d4b5226
JeonJungyu <wjswnsrb93@naver.com>
2023-08-14 17:11:04 +0900

    feat : 알림 임시 추가

--- src/components/SSE.tsx
+++ src/components/SSE.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { getConfig, post } from '@/service/api/http';
 import { useEffect, useState } from 'react';
+import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
 
 export default function SSE() {
   const [listening, setListening] = useState(false);
@@ -8,35 +10,80 @@ export default function SSE() {
 
   useEffect(() => {
     let eventSource: EventSource | undefined = undefined;
+    const EventSource = EventSourcePolyfill || NativeEventSource;
+
     if (!listening) {
-      eventSource = new EventSource(`${process.env.NEXT_PUBLIC_BACKEND_URL}/alarms/topic//subscribe`);
-        
-      eventSource.addEventListener('Progress', (e) => {
+      // eventSource = new EventSource(`http://localhost:8080/time`);
+    //   eventSource = new EventSource(
+    //     `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/notifications/subscribe`,
+    //     {
+    //       headers: {
+    //         Authorization:
+    //           localStorage && localStorage.getItem('accessToken')
+    //             ? `Bearer ${localStorage.getItem('accessToken')}`
+    //             : '',
+    //       },
+    //       withCredentials: true,
+    //     },
+    //   );
+        eventSource = new EventSource(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/alarms`,
+          {
+            headers: {
+              Authorization:
+                localStorage && localStorage.getItem('accessToken')
+                  ? `Bearer ${localStorage.getItem('accessToken')}`
+                  : '',
+            },
+            withCredentials: true,
+          },
+        );
+
+      eventSource?.addEventListener('Progress', (e) => {
         const result = JSON.parse(e.data);
         console.log('received:', result);
         // setData(result);
       });
 
-      eventSource.onerror = (e) => {
-        const event = e.target as EventSource;
-        if (event.readyState === EventSource.CLOSED) {
-          console.log('SSE closed (' + event.readyState + ')');
-        }
-        event.close();
-      };
+      if (eventSource) {
+        eventSource.onmessage = (event) => {
+          console.log('result', event.data);
+          // setData(old => [...old, event.data])
+        };
 
-      eventSource.onopen = (e) => {
-        console.log('connection opened');
-      };
+        eventSource.onerror = (e) => {
+          const event = e.target as EventSource;
+          if (event.readyState === EventSource.CLOSED) {
+            console.log('SSE closed (' + event.readyState + ')');
+          }
+          event.close();
+        };
+
+        eventSource.onopen = (e) => {
+          console.log('connection opened');
+        };
+      }
 
       setListening(true);
     }
 
     return () => {
       eventSource?.close();
-      console.log("event losed")
+      console.log('event closed');
     };
-  }, [listening]);
+  }, []);
 
-  return <div>SSE</div>;
+  return (
+    <div>
+      SSE
+      <button
+        onClick={async () => {
+          const res = await post('/api/notifications/send-data');
+          console.log(res);
+        }}
+      >
+        신호
+      </button>
+    </div>
+  );
 }
