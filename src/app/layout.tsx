33c6cfa3ebb2224c2a871174efdbc108d7b32071
JeonJungyu <wjswnsrb93@naver.com>
2023-08-14 09:45:40 +0900

    feat : http 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className={spokaFont.className}>
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className="overflow-hidden m-0 p-0 ">
         <QueryProvider>
           <AuthContextProvider>
