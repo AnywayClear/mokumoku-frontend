e2b1c40f2b5921d3156d965a28b98f5426c9329e
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-08-16 01:20:13 +0900

    refactor: sse 코드 정리

--- src/components/SSE.tsx
+++ src/components/SSE.tsx
@@ -3,75 +3,12 @@
 import { getConfig, post } from '@/service/api/http';
 import { useEffect, useState } from 'react';
 import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
+import { toast } from 'react-toastify';
 
 export default function SSE() {
   const [listening, setListening] = useState(false);
   const [data, setData] = useState({ value: 0, target: 100 });
 
-  //   useEffect(() => {
-  //     let eventSource: EventSource | undefined = undefined;
-  //     const EventSource = EventSourcePolyfill || NativeEventSource;
-
-  //     if (!listening) {
-  //       // eventSource = new EventSource(`http://localhost:8080/time`);
-  //       eventSource = new EventSource(
-  //         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/notifications/subscribe`,
-  //         {
-  //           headers: {
-  //             Authorization:
-  //               localStorage && localStorage.getItem('accessToken')
-  //                 ? `Bearer ${localStorage.getItem('accessToken')}`
-  //                 : '',
-  //           },
-  //           withCredentials: true,
-  //         },
-  //       );
-  //         // eventSource = new EventSource(
-  //         //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/alarms`,
-  //         //   {
-  //         //     headers: {
-  //         //       Authorization:
-  //         //         localStorage && localStorage.getItem('accessToken')
-  //         //           ? `Bearer ${localStorage.getItem('accessToken')}`
-  //         //           : '',
-  //         //     },
-  //         //     withCredentials: true,
-  //         //   },
-  //         // );
-
-  //       eventSource?.addEventListener('Progress', (e) => {
-  //         const result = JSON.parse(e.data);
-  //         console.log('received:', result);
-  //         // setData(result);
-  //       });
-
-  //       if (eventSource) {
-  //         eventSource.onmessage = (event) => {
-  //           console.log('result', event.data);
-  //           // setData(old => [...old, event.data])
-  //         };
-
-  //         eventSource.onerror = (e) => {
-  //           const event = e.target as EventSource;
-  //           if (event.readyState === EventSource.CLOSED) {
-  //             console.log('SSE closed (' + event.readyState + ')');
-  //           }
-  //           event.close();
-  //         };
-
-  //         eventSource.onopen = (e) => {
-  //           console.log('connection opened');
-  //         };
-  //       }
-
-  //       setListening(true);
-  //     }
-
-  //     return () => {
-  //       eventSource?.close();
-  //       console.log('event closed');
-  //     };
-  //   }, []);
 
   useEffect(() => {
     //     let eventSource: EventSource | undefined = undefined;
@@ -89,55 +26,49 @@ export default function SSE() {
       },
     );
 
-    eventSource.addEventListener('sse', function (event:any) {
+    eventSource.addEventListener('sse', function (event: any) {
       console.log(event.data);
 
-      const data = JSON.parse(event.data);
+      // const data = JSON.parse(event.data);
+      const data = event.data;
+      console.log(event);
 
-      (async () => {
-        // 브라우저 알림
-        const showNotification = () => {
-          const notification = new Notification('코드 봐줘', {
-            body: data.content,
-          });
+      toast.info(data, {
+        autoClose: false,
+      });
+      // (async () => {
+      //   // 브라우저 알림
+      //   const showNotification = () => {
+      //     const notification = new Notification('코드 봐줘', {
+      //       body: data.content,
+      //     });
 
-          setTimeout(() => {
-            notification.close();
-          }, 10 * 1000);
+      //     setTimeout(() => {
+      //       notification.close();
+      //     }, 10 * 1000);
 
-          notification.addEventListener('click', () => {
-            window.open(data.url, '_blank');
-          });
-        };
+      //     notification.addEventListener('click', () => {
+      //       window.open(data.url, '_blank');
+      //     });
+      //   };
 
-        // 브라우저 알림 허용 권한
-        let granted = false;
+      //   // 브라우저 알림 허용 권한
+      //   let granted = false;
 
-        if (Notification.permission === 'granted') {
-          granted = true;
-        } else if (Notification.permission !== 'denied') {
-          let permission = await Notification.requestPermission();
-          granted = permission === 'granted';
-        }
+      //   if (Notification.permission === 'granted') {
+      //     granted = true;
+      //   } else if (Notification.permission !== 'denied') {
+      //     let permission = await Notification.requestPermission();
+      //     granted = permission === 'granted';
+      //   }
 
-        // 알림 보여주기
-        if (granted) {
-          showNotification();
-        }
-      })();
+      //   // 알림 보여주기
+      //   if (granted) {
+      //     showNotification();
+      //   }
+      // })();
     });
   }, []);
 
-  return (
-    <div>
-      SSE
-      <button
-        onClick={() => {
-          post('/api/notifications/send-data');
-        }}
-      >
-        신호
-      </button>
-    </div>
-  );
+  return <></>;
 }
