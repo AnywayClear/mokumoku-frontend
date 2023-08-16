5d439a62d95f0bf72bd9932f465fe769d9cc8530
ssh102525 <ssh102525@naver.com>
2023-08-16 10:41:06 +0900

    feat: 날짜 문자열 변환 함수 종류 추가

--- src/myFunc.ts
+++ src/myFunc.ts
@@ -1,4 +1,12 @@
-export function dateToString(date: Date): string { 
+export function dateToStringDot(date: Date): string { 
+    let newDate = new Date(date);
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, '0');
+    const day = String(newDate.getDate()).padStart(2, '0');
+
+    return `${year}.${month}.${day}`;
+}
+export function dateToStringDash(date: Date): string { 
     let newDate = new Date(date);
     const year = newDate.getFullYear();
     const month = String(newDate.getMonth() + 1).padStart(2, '0');
