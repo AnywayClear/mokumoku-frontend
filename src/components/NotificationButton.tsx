8afaffc763d842eba15963e4ab7ba1aaa7e917e8
JeonJungyu <wjswnsrb93@naver.com>
2023-08-16 17:57:27 +0900

    feat : 알림 리스트 기능 추가

---
+++ src/components/NotificationButton.tsx
@@ -0,0 +1,164 @@
+'use client';
+
+import { get } from '@/service/api/http';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+// type : 0 - 농산물, 1 - 판매자, 2 - 낙찰
+type Notification = {
+  type: number;
+  senderId: string;
+  senderName: string;
+};
+
+export default function NotificationButton() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prev) => !prev);
+  };
+
+  const { data: produce }: UseQueryResult<Notification[]> = useQuery({
+    queryKey: ['alarm'],
+    queryFn: () => get('/api/notifications/list'),
+
+  });
+
+  return (
+    <div className="flex justify-center">
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="relative z-10 block rounded-md bg-white p-2 focus:outline-none"
+        >
+          <svg
+            className="h-5 w-5 text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+          </svg>
+        </button>
+
+        {dropdownOpen && (
+          <div
+            onClick={toggleDropdown}
+            className="fixed inset-0 h-full w-full z-10"
+          ></div>
+        )}
+
+        {dropdownOpen && (
+          <div
+            className="absolute right-0 mt-2 bg-white rounded-md shadow-lg overflow-hidden z-20"
+            style={{ width: '20rem' }}
+          >
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <img
+                  className="h-8 w-8 rounded-full object-cover mx-1"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                  alt="avatar"
+                />
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <img
+                  className="h-8 w-8 rounded-full object-cover mx-1"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                  alt="avatar"
+                />
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <img
+                  className="h-8 w-8 rounded-full object-cover mx-1"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                  alt="avatar"
+                />
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <img
+                  className="h-8 w-8 rounded-full object-cover mx-1"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                  alt="avatar"
+                />
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <div className="py-2">
+              <a
+                href="#"
+                className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2"
+              >
+                <img
+                  className="h-8 w-8 rounded-full object-cover mx-1"
+                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                  alt="avatar"
+                />
+                <p className="text-gray-600 text-sm mx-2">
+                  <span className="font-bold">Sara Salah</span> replied on the{' '}
+                  <span className="font-bold text-blue-500">Upload Image</span>{' '}
+                  article. 2m
+                </p>
+              </a>
+            </div>
+            <a
+              href="#"
+              className="block bg-gray-800 text-white text-center font-bold py-2"
+            >
+              See all notifications
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
