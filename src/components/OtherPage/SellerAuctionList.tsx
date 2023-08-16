031cd6dbee8dafed18fd38cbbaa56a77396990f3
ssh102525 <ssh102525@naver.com>
2023-08-16 17:57:31 +0900

    feat: 타인 페이지 판매 목록 결제 완료 탭 추가를 위한 search tap 설정

--- src/components/OtherPage/SellerAuctionList.tsx
+++ src/components/OtherPage/SellerAuctionList.tsx
@@ -1,13 +1,17 @@
 'use client';
 import * as React from 'react';
 import SearchTab from '../MyPage/searchTab/searchTab';
-import { useRecoilState} from 'recoil';
-import { searchState} from '@/store/mypage';
+import { useRecoilState } from 'recoil';
+import { searchState } from '@/store/mypage';
 import SellerAuctionRow from './SellerAuctionRow';
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import { ProduceList } from '@/model/produce';
 import { getProduceList2 } from '@/service/api/produce';
 import { searchType } from '@/model/mypage';
+import { getDealList, getDealList2 } from '@/service/api/deal';
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
+import { DealList } from '@/model/deal';
 
 type colType = { name: string; flex: string };
 const cols: colType[] = [
@@ -42,20 +46,31 @@ const cols: colType[] = [
 ];
 
 type Props = {
-  slug: string
-}
-export default function SellerAuctionList({slug}: Props) {
-
+  slug: string;
+};
+export default function SellerAuctionList({ slug }: Props) {
+  const { user } = useContext(AuthContext);
   const [{ auctionState, title }] = useRecoilState<searchType>(searchState);
 
   const { data: produceList }: UseQueryResult<ProduceList> = useQuery({
-    queryKey: ['produceList',auctionState, title],
+    queryKey: ['produceList', auctionState, title],
     queryFn: () => getProduceList2(auctionState.toString(), title, slug, 0, 10),
+  });
+  // consumerId: string | undefined,
+  // startDate: String,
+  // endDate: string,
+  // page: number,
+  // size: number,
+  const { data: dealList }: UseQueryResult<DealList> = useQuery({
+    queryKey: ['dealList', auctionState],
+    queryFn: () => getDealList2(user?.userId, 0, 10),
   });
 
   return (
     <div className="mb-20">
-      <SearchTab tabType={1} />
+      {
+        auctionState[0]===0?<SearchTab tabType={1} />:<SearchTab tabType={4} />
+      }
       <div>
         <table className="table-fixed border-collapse border-y-2 w-full text-center border-neutral-300">
           <thead className="font-bold  text-xl">
@@ -68,13 +83,43 @@ export default function SellerAuctionList({slug}: Props) {
             </tr>
           </thead>
           <tbody>
-            {produceList?.data.length!==0&&produceList!==undefined?(produceList?.data?.map((produce, index) => (
-              produce.auctionResponseList.map((auction, index2) => (
-                <SellerAuctionRow key={index2} produce={produce} auction={auction} />
-              ))
-            ))):
-            <tr className='h-32'><td className="text-xl font-semibold" colSpan={cols.length}>검색된 게시물이 없습니다.</td></tr>
-          }
+            {auctionState[0] !== 3 ? (
+              produceList?.data.length !== 0 && produceList !== undefined ? (
+                produceList?.data?.map((produce, index) =>
+                  produce.auctionResponseList.map((auction, index2) => (
+                    <SellerAuctionRow
+                      key={index2}
+                      produce={produce}
+                      auction={auction}
+                    />
+                  )),
+                )
+              ) : (
+                <tr className="h-32">
+                  <td className="text-xl font-semibold" colSpan={cols.length}>
+                    검색된 게시물이 없습니다.
+                  </td>
+                </tr>
+              )
+            ) : (
+              produceList?.data.length !== 0 && produceList !== undefined ? (
+                produceList?.data?.map((produce, index) =>
+                  produce.auctionResponseList.map((auction, index2) => (
+                    <SellerAuctionRow
+                      key={index2}
+                      produce={produce}
+                      auction={auction}
+                    />
+                  )),
+                )
+              ) : (
+                <tr className="h-32">
+                  <td className="text-xl font-semibold" colSpan={cols.length}>
+                    검색된 게시물이 없습니다.
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
         </table>
       </div>
