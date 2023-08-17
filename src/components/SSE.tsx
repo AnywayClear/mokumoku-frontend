42aa793dfef0b4d1396c981af0e896f535f4a021
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-08-18 00:20:39 +0900

    fix : sse 토큰 있을때만 연결하게 수정

--- src/components/SSE.tsx
+++ src/components/SSE.tsx
@@ -28,76 +28,87 @@ export default function SSE() {
 
   useEffect(() => {
     //     let eventSource: EventSource | undefined = undefined;
-    const EventSource = EventSourcePolyfill || NativeEventSource;
-    const eventSource = new EventSource(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/notifications/subscribe`,
-      {
-        headers: {
-          Authorization:
-            localStorage && localStorage.getItem('accessToken')
-              ? `Bearer ${localStorage.getItem('accessToken')}`
-              : '',
-        },
-        // heartbeatTimeout: 1200000,
-        withCredentials: true,
-      },
-    );
+    function checkUserData() {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        const EventSource = EventSourcePolyfill || NativeEventSource;
+        const eventSource = new EventSource(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/notifications/subscribe`,
+          {
+            headers: {
+              Authorization:
+                localStorage && localStorage.getItem('accessToken')
+                  ? `Bearer ${localStorage.getItem('accessToken')}`
+                  : '',
+            },
+            // heartbeatTimeout: 1200000,
+            withCredentials: true,
+          },
+        );
 
-    eventSource.addEventListener('sse', function (event: any) {
-      console.log(event);
+        eventSource.addEventListener('sse', function (event: any) {
+          console.log(event);
 
-      const data: Alarm = JSON.parse(event.data);
-      // const data = event.data;
-      // console.log(event);
+          const data: Alarm = JSON.parse(event.data);
+          // const data = event.data;
+          // console.log(event);
 
-      if (event.type !== 'connection') {
-        const messageData = getAlarmMessage(data);
-        toast.info(messageData.message, {
-          autoClose: false,
-          // closeButton: (data) => CloseButton(data, messageData.link),
+          if (event.type !== 'connection') {
+            const messageData = getAlarmMessage(data);
+            toast.info(messageData.message, {
+              autoClose: false,
+              // closeButton: (data) => CloseButton(data, messageData.link),
+            });
+            queryClient.invalidateQueries({ queryKey: ['alarm'] });
+          }
+          // (async () => {
+          //   // 브라우저 알림
+          //   const showNotification = () => {
+          //     const notification = new Notification('코드 봐줘', {
+          //       body: data.content,
+          //     });
+
+          //     setTimeout(() => {
+          //       notification.close();
+          //     }, 10 * 1000);
+
+          //     notification.addEventListener('click', () => {
+          //       window.open(data.url, '_blank');
+          //     });
+          //   };
+
+          //   // 브라우저 알림 허용 권한
+          //   let granted = false;
+
+          //   if (Notification.permission === 'granted') {
+          //     granted = true;
+          //   } else if (Notification.permission !== 'denied') {
+          //     let permission = await Notification.requestPermission();
+          //     granted = permission === 'granted';
+          //   }
+
+          //   // 알림 보여주기
+          //   if (granted) {
+          //     showNotification();
+          //   }
+          // })();
         });
-        queryClient.invalidateQueries({ queryKey: ['alarm'] });
+        eventSource.onmessage = (error) => {
+          console.log(error);
+        };
+
+        eventSource.onerror = (error) => {
+          // console.log(error);
+        };
       }
-      // (async () => {
-      //   // 브라우저 알림
-      //   const showNotification = () => {
-      //     const notification = new Notification('코드 봐줘', {
-      //       body: data.content,
-      //     });
+    }
 
-      //     setTimeout(() => {
-      //       notification.close();
-      //     }, 10 * 1000);
+    window.addEventListener('storage', checkUserData);
 
-      //     notification.addEventListener('click', () => {
-      //       window.open(data.url, '_blank');
-      //     });
-      //   };
-
-      //   // 브라우저 알림 허용 권한
-      //   let granted = false;
-
-      //   if (Notification.permission === 'granted') {
-      //     granted = true;
-      //   } else if (Notification.permission !== 'denied') {
-      //     let permission = await Notification.requestPermission();
-      //     granted = permission === 'granted';
-      //   }
-
-      //   // 알림 보여주기
-      //   if (granted) {
-      //     showNotification();
-      //   }
-      // })();
-    });
-    eventSource.onmessage = (error) => {
-      console.log(error);
+    return () => {
+      window.removeEventListener('storage', checkUserData);
     };
-
-    eventSource.onerror = (error) => {
-      // console.log(error);
-    };
-  }, [router]);
+  }, [queryClient, router]);
 
   return <></>;
 }
